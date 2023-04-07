6b35cc5b837e874044fe800d4f272d903c5fd7a8
JuanSebastianGaleanoGonzalez <71532153+JuanSebastianGaleanoGonzalez@users.noreply.github.com>
2023-04-07 01:32:25 -0500

    Commit Inicial
    Creacion del proyecto Spring para ProteSys Web.

---
+++ ProteSysWeb/src/test/java/com/protesys/ProteSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
