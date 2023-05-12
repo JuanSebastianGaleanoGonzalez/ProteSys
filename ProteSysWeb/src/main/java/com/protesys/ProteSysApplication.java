0abd8b75e99be63c3e7e4e3110f67dd443da02f7
Sebastian Galeano Gonzalez <jusebastiangg@gmail.com>
2023-05-12 01:56:09 -0500

    Firebase implementado.

--- ProteSysWeb/src/main/java/com/protesys/ProteSysApplication.java
+++ ProteSysWeb/src/main/java/com/protesys/ProteSysApplication.java
@@ -2,8 +2,9 @@ package com.protesys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class ProteSysApplication {
 
 	public static void main(String[] args) {
---
+++ ProteSysWeb/src/main/java/com/protesys/dto/UsuarioDTO.java
@@ -0,0 +1,11 @@
+package com.protesys.dto;
+
+import com.protesys.model.Rol;
+
+public class UsuarioDTO {
+    private String id;
+    private long cedula;
+    private String nombre;
+    private String correo;
+    private long telefono;
+}
