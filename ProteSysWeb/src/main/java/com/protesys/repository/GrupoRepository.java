e1c9f1701be0a358a7cb120210198d999778fd0b
Sebastian Galeano Gonzalez <jusebastiangg@gmail.com>
2023-05-15 10:47:53 -0500

    Creacion de los Repositorios JPA

---
+++ ProteSysWeb/src/main/java/com/protesys/repository/GrupoRepository.java
@@ -0,0 +1,8 @@
+package com.protesys.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.protesys.model.Grupo;
+
+public interface GrupoRepository extends CrudRepository<Grupo, Long>{ 
+}
---
+++ ProteSysWeb/src/main/java/com/protesys/repository/PisoRepository.java
@@ -0,0 +1,7 @@
+package com.protesys.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PisoRepository extends CrudRepository<Piso, Long>{
+    
+}
