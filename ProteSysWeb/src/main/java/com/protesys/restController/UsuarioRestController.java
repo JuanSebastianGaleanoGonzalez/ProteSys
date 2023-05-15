66cecda1706882b326a10375786b2d727df48513
Sebastian Galeano Gonzalez <jusebastiangg@gmail.com>
2023-05-15 12:31:43 -0500

    Creado archivo de configuracion de CORS

--- ProteSysWeb/src/main/java/com/protesys/restController/UsuarioRestController.java
+++ ProteSysWeb/src/main/java/com/protesys/restController/UsuarioRestController.java
@@ -1,5 +1,17 @@
 package com.protesys.restController;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.protesys.services.UsuarioService;
+
+@RestController
+@RequestMapping(value = "/usuario")
 public class UsuarioRestController {
-    
+
+    @Autowired
+    UsuarioService usuarioService;
+
+     
 }
