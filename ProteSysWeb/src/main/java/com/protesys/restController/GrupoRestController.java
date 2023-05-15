5824457dafa3fb64cb91f96539630fe64b60e702
Sebastian Galeano Gonzalez <jusebastiangg@gmail.com>
2023-05-15 12:49:51 -0500

    RestController Grupo Completado

--- ProteSysWeb/src/main/java/com/protesys/restController/GrupoRestController.java
+++ ProteSysWeb/src/main/java/com/protesys/restController/GrupoRestController.java
@@ -1,5 +1,45 @@
 package com.protesys.restController;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.protesys.model.Grupo;
+import com.protesys.services.GrupoService;
+
 public class GrupoRestController {
     
+    @Autowired
+    GrupoService grupoService;
+
+    @GetMapping(value = "/search")
+    public List<Grupo> listarGrupos() {
+        return this.grupoService.getGrupos();
+    }
+
+    @GetMapping(value = "/search/{id}")
+    public Grupo buscarGrupo(@PathVariable("id") Long id) {
+        return this.grupoService.getGrupo(id);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public boolean eliminarGrupo(@PathVariable("id") Long id) {
+        return this.grupoService.deleteGrupo(id);
+    }
+
+    @PostMapping(value = "/create")
+    public boolean crearGrupo(@RequestBody Grupo grupo) {
+        return this.grupoService.createGrupo(grupo);
+    }
+
+    @PutMapping(value = "/update")
+    public boolean actualizarGrupo(@RequestBody Grupo grupo) {
+        return this.grupoService.updateGrupo(grupo);
+    }
 }
--- ProteSysWeb/src/main/java/com/protesys/restController/PisoRestController.java
+++ ProteSysWeb/src/main/java/com/protesys/restController/PisoRestController.java
@@ -9,37 +9,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.protesys.model.Piso;
 import com.protesys.services.PisoService;
 
+@RestController
+@RequestMapping(value = "/piso")
 public class PisoRestController {
-    
-@Autowired
-PisoService pisoService;
 
-@GetMapping(value = "/search")
-public List<Piso> listarPisos(){
-return this.pisoService.getPisos();
-}
+    @Autowired
+    PisoService pisoService;
 
-@GetMapping(value = "/search/{id}")
-public Piso buscarPiso(@PathVariable ("id") Long id){
-return this.pisoService.getPiso(id);
-}
+    @GetMapping(value = "/search")
+    public List<Piso> listarPisos() {
+        return this.pisoService.getPisos();
+    }
 
-@DeleteMapping(value = "/delete/{id}")
-public boolean eliminarPiso(@PathVariable ("id") Long id){
-return this.pisoService.deletePiso(id);
-}
+    @GetMapping(value = "/search/{id}")
+    public Piso buscarPiso(@PathVariable("id") Long id) {
+        return this.pisoService.getPiso(id);
+    }
 
-@PostMapping(value = "/create")
-public boolean crearPiso(@RequestBody Piso piso){
-return this.pisoService.createPiso(piso);
-}
+    @DeleteMapping(value = "/delete/{id}")
+    public boolean eliminarPiso(@PathVariable("id") Long id) {
+        return this.pisoService.deletePiso(id);
+    }
 
-@PutMapping(value = "/update")
-public boolean actualizarPiso(@RequestBody Piso piso){
-return this.pisoService.updatePiso(piso);
-}
+    @PostMapping(value = "/create")
+    public boolean crearPiso(@RequestBody Piso piso) {
+        return this.pisoService.createPiso(piso);
+    }
+
+    @PutMapping(value = "/update")
+    public boolean actualizarPiso(@RequestBody Piso piso) {
+        return this.pisoService.updatePiso(piso);
+    }
 }
